cf82e02ab3f06dde3374fb3e92efbcfdc63addca
angelwshotgun <115882828+angelwshotgun@users.noreply.github.com>
2024-07-18 03:48:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ genai.configure(api_key=api_key)
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def serve_frontend():
     return render_template('index.html')
 
 @app.route('/generate_text', methods=['POST'])
@@ -32,10 +32,12 @@ def generate_text():
     
     model = genai.GenerativeModel('gemini-1.5-flash')
     
-    response = model.generate_content(["bạn đọc được những gì", image], stream=True,
+    response = model.generate_content(["read text in format: ID:, Country: ISO 3166-1 alpha-3, Name:, Gender:, Date of Birth: dd/mm/yyyy", image], stream=True,
                                       safety_settings={
         HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.block,
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
     })
     response.resolve()
     
