e6e6c5c2e7968e80a43db61af8e847d7b587c0d5
angelwshotgun <115882828+angelwshotgun@users.noreply.github.com>
2024-10-14 16:48:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -45,5 +45,63 @@ def generate_text():
     })
     response.resolve()
     return response.text
+
+@app.route('/generate_text2', methods=['POST'])
+def generate_text2():
+    if 'text' not in request.json:
+        return jsonify({'error': 'No text provided'}), 400
+    
+    input_text = request.json['text']
+    
+    generation_config = {
+        "temperature": 1,
+        "top_p": 0.95,
+        "top_k": 64,
+        "max_output_tokens": 8192,
+        "response_mime_type": "text/plain",
+    }
+
+    model = genai.GenerativeModel(
+        model_name="gemini-1.5-pro",
+        generation_config=generation_config,
+        system_instruction="""Result only
+My requirements are:
+
+ID: get from header after booking number
+the number day booking CLD if room booked is Classic Double Room, CLP if Classic Premium Room, STU if Studio Room, TRIP if Triple Room, (dd - dd/mm)
+NET: Calculate the number of nights between check-in and check-out dates. Then, divide the total net price by the number of nights to get the daily net price. Format the result as follows: Perform the division without rounding. Do not include any currency symbols. Use a period (.) as the thousands separator
+Add '(Prepaid)' at the end
+
+Tên: uppercase name
+Total net: is total net
+example
+ID: 1234567890
+1 CLP (02 - 04/12)
+NET: ??? (Prepaid)
+
+Tên: Nas
+Total net: 600.000"""
+    )
+
+    chat_session = model.start_chat(
+        history=[
+            {
+                "role": "user",
+                "parts": [
+                    "New Booking from Agoda | AG169D1728475852N3405 (1395998868)\nHộp thư đến\nNội dung cập nhật\n\nno-reply do-not-reply@hotellinksolutions.com\n19:10 Th 4, 9 thg 10 (5 ngày trước)\nđến tôi\n\nDAHLIA HOTEL HANOI\n\nCONGRATULATIONS! You've received a new booking.\nBooking number: AG169D1728475852N3405\nAgoda has sent the confirmation email to the guest.\nFor booking enquiries, cancellations or amendments the guest has been instructed to contact Agoda directly.\nVIEW BOOKING\nYOUR BOOKING\nGuest:    Lesseree Rose Santos (Philippines)\n63 09175045779\nCheck-in:    Saturday, October 12, 2024 from 14:00\nCheck-out:    Tuesday, October 15, 2024 until 12:00\nRooms booked:    1 Studio - Non-refundable\nBooked on:    Wednesday, October 9, 2024\nSource:    Agoda (Agoda Handles the Payment) (booking details)\nTotal net price:    ₫ 3.538.080\nTotal sell price:    ₫ 4.914.000",
+                ],
+            },
+            {
+                "role": "model",
+                "parts": [
+                    "ID: 1395998868\n1 STU (12 - 15/10)\nNET: 1.179.360 (Prepaid)\n\nTên: ROSE SANTOS \nTotal net: 3.538.080 \n",
+                ],
+            },
+        ]
+    )
+    
+    response = chat_session.send_message(input_text)
+    return response.text
+    
 if __name__ == '__main__':
     app.run(debug=True)
